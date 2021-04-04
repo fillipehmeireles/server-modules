ad8de9d4457c67c63f410fdfe236896b83cfc6cd
fillipehmeireles <hebriquefillipe@gmail.com>
2021-04-04 09:27:22 -0300

    [FileMonitor] persistent watcher

--- FileMonitor/fileMonitor.c
+++ FileMonitor/fileMonitor.c
@@ -4,16 +4,28 @@
         - fileMonitor.c
 
     ToDo:
-        - Persistent Event Watcher
         - Browser/Mobile notifier
 
     Author: Fillipe Meireles
     Organization: Luspew
 
     https://github.com/fillipehmeireles/server-modules
-    
+
 ***/
 
+/* 
+
+    void logger(LOGGER_MSG);
+
+    log_type = [
+        'w', 
+        'e',
+        'i'
+    ]
+
+    -> WARNING, ERROR, INFO
+
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,12 +37,33 @@
 #define EVENT_BUFF_SIZE (sizeof(struct inotify_event))
 #define BUFFER_SIZE (1024 * (EVENT_BUFF_SIZE + 16))
 
+typedef struct
+{
+    char *type;
+    char *msg;
+
+} LOGGER_MSG;
+
+void logger(LOGGER_MSG log);
+
+void test(char *c, char *r)
+{
+    printf("%s", c);
+}
 int main(int argc, char *argv[])
 {
+    LOGGER_MSG log;
 
     if (argc < 2)
     {
-        printf("[!]Please, provide the directory path.\n");
+
+        char *msg_type = "e";
+        char *msg = "Please, provide the directory path.";
+
+        log.msg = msg;
+        log.type = msg_type;
+
+        logger(log);
         return -1;
     }
 
@@ -48,51 +81,63 @@ int main(int argc, char *argv[])
 
     int buff_read = 0;
     dir_watcher = inotify_add_watch(fd, argv[1], IN_MODIFY | IN_CREATE | IN_DELETE);
-    buff_read = read(fd, buffer, BUFFER_SIZE);
-    if (buff_read == -1)
+
+    while (1)
     {
-        perror("Could not read in buffer");
-        return -1;
+        struct inotify_event *event, *end;
+        buff_read = read(fd, buffer, BUFFER_SIZE);
+
+        if (buff_read < 0)
+        {
+            perror("read");
+            break;
+        }
+
+        event = (struct inotify_event *)&buffer[0];
+
+        end = (struct inotify_event *)&buffer[buff_read];
+
+        while (event < end)
+        {
+            if (event->mask & IN_CREATE)
+            {
+                if (event->mask & IN_ISDIR)
+                {
+
+                    printf("Directory %s was created\n", event->name);
+                }
+                else
+                {
+                    printf("[+] File %s was created.\n", event->name);
+                }
+            }
+            if (event->mask & IN_MODIFY)
+            {
+                printf("[+-] File %s was modified.\n", event->name);
+            }
+            if (event->mask & IN_DELETE)
+            {
+                if (event->mask & IN_ISDIR)
+                {
+                    printf("Directory %s was deleted\n", event->name);
+                }
+                else
+                {
+
+                    printf("[-] File %s was deleted.\n", event->name);
+                }
+            }
+            event = (struct inotify_event *)((char *)event) + sizeof(*event) + event->len;
+        }
     }
+}
 
-    int i = 0;
-
-    while (i < buff_read)
-    {
-        struct inotify_event *event = (struct inotify_event *)&buffer[i];
-
-        if (event->mask & IN_CREATE)
-        {
-            if (event->mask & IN_ISDIR)
-            {
-                printf("Directory %s was created\n", event->name);
-            }
-            else
-            {
-
-                printf("[+] File %s was created.\n", event->name);
-            }
-        }
-        if (event->mask & IN_MODIFY)
-        {
-            printf("[+-] File %s was modified.\n", event->name);
-        }
-        if (event->mask & IN_DELETE)
-        {
-            if (event->mask & IN_ISDIR)
-            {
-                printf("Directory %s was deleted\n", event->name);
-            }
-            else
-            {
-
-                printf("[-] File %s was deleted.\n", event->name);
-            }
-        }
-
-        i += EVENT_BUFF_SIZE + event->len;
-    }
-
-    inotify_rm_watch(fd, dir_watcher);
-    close(fd);
+void logger(LOGGER_MSG log)
+{
+    if (*log.type == 'w')
+        printf("[!] %s\n", log.msg);
+    if (*log.type == 'e')
+        printf("[X] %s\n", log.msg);
+    if (*log.type == 'i')
+        printf("[*] %s\n", log.msg);
 }
